f4b432dae93efc459dbca5bdc5b4b4a22449937f
vankraf <59446697+vankraf@users.noreply.github.com>
2020-04-05 22:24:14 +0300

    static

---
+++ precache-manifest.d09847e399d7482111adb1f8b70b9c12.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5ecb7e46e4c297d0235bdcb39aa39a90",
+    "url": "/new/index.html"
+  },
+  {
+    "revision": "f3bf65e2bbeceabeac1c",
+    "url": "/new/static/css/2.4fc5a6bc.chunk.css"
+  },
+  {
+    "revision": "6a28fae676cc00c76cbb",
+    "url": "/new/static/css/main.981f7211.chunk.css"
+  },
+  {
+    "revision": "f3bf65e2bbeceabeac1c",
+    "url": "/new/static/js/2.8324e4c4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/new/static/js/2.8324e4c4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6a28fae676cc00c76cbb",
+    "url": "/new/static/js/main.a92e5869.chunk.js"
+  },
+  {
+    "revision": "7fbeb5c339f46b5a162c",
+    "url": "/new/static/js/runtime-main.b3e4ed86.js"
+  },
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "/new/static/media/persik.4e1ec840.png"
+  }
+]);
